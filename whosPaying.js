6aa620c5c12497f8307298184b72bd7d22612503
Dominik Maciazek <114284333+DominikMaciazek@users.noreply.github.com>
2023-01-12 03:35:07 +0100

    Add files via upload

---
+++ whosPaying.js
@@ -0,0 +1,13 @@
+function whosPaying(names) {
+    //   function choose random person who is going to buy lunch today
+        
+       var names=  ["Angela", "Ben", "Jenny", "Michael", "Chloe"];
+       var chosenName = Math.floor(Math.random() * names.length);
+       var result = names [chosenName]
+        return (result + " is going to buy lunch today!")
+        
+    
+       
+    }
+    
+    whosPaying()
